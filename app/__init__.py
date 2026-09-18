31565b6a6bf4da4ed602780abdbef658d9b96bb1
Saad-Sid85 <saadsiddiqui1905@gmail.com>
2026-09-18 20:40:58 +0530

    Initial StudyGenie project setup

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder="../templates",
+        static_folder="../static"
+    )
+
+    app.config["SECRET_KEY"] = "studygenie-secret-key"
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
